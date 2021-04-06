57d7bcd88147a11fa49468d39ac54ce0e1b419b4
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-04-07 00:31:43 +0900

    Add sendEmall sample

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 function myFunction() {
-  Logger.log('Hello World');
+  Logger.log(PropertiesService.getScriptProperties().getProperty('to'));
+  Logger.log(PropertiesService.getScriptProperties().getProperty('from'));
+  Logger.log(PropertiesService.getScriptProperties().getProperty('cc'));
+}
+
+
+// メールを送る
+function sendMail() {
+  var mailto = PropertiesService.getScriptProperties().getProperty('to');
+  var mailfrom = PropertiesService.getScriptProperties().getProperty('from');
+  var mailcc = PropertiesService.getScriptProperties().getProperty('cc');
+  var subject = 'This is only a test.';
+  var date = new Date();
+  date.toLocaleString("ja");
+  var body = 'このメールはテストメールです。送信日時：' + date.toString();
+  try {
+    GmailApp.sendEmail(mailto, subject, body, {
+      cc: mailcc,
+      from: mailfrom
+    });
+  } catch(e) {
+     Logger.log(e);
+  }
 }
